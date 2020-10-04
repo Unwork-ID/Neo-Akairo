134f7631ccb367d43e802bbb308dda4b0adeb59b
tokisaki13 <lintanggaming24@gmail.com>
2020-10-04 13:55:26 +0700

    update: Added Skip and Fixing Stop Command

--- src/NeoCommandsFiles/Music/SkipCommand.ts
+++ src/NeoCommandsFiles/Music/SkipCommand.ts
@@ -3,17 +3,17 @@ import Client from '../../Client/NeoClient';
 import { Command } from 'discord-akairo';
 import { Message } from 'discord.js';
 
-export default class StopCommand extends Command {
+export default class SkipCommand extends Command {
     client: Client
     public constructor(client: Client) {
-        super("stop", {
-            aliases: ["stop"],
+        super("skip", {
+            aliases: ["skip"],
             category: "Music",
             description: {
                 content: "To stop current music",
-                usage: "stop",
+                usage: "skip",
                 example: [
-                    "stop"
+                    "skip"
                 ]
             },
             ratelimit: 3e5,
@@ -28,9 +28,9 @@ export default class StopCommand extends Command {
         if (!player) return message.reply(`This guild currently not playing anything`);
 
         const is = player.queue.current.requester === message.author.id || message.member.hasPermission(['MANAGE_MESSAGES']);
-        if (!is) return message.reply(`Only requester and admin can stop the music`);
+        if (!is) return message.reply(`Only requester and admin can skip the music`);
 
-        player.destroy();
-        return message.channel.send(`Music Stop by: <@${message.author.id}>`);
+        player.stop();
+        return message.channel.send(`Muisc Skip by: <@${message.author.id}>`);
     }
 }
